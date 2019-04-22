db3b2937078b045cf8c3b05f2795f0a8f185f704
raberin <rmvaberin@gmail.com>
2019-04-21 17:18:29 -0700

    Added Fetch data action reducer

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,8 +1,16 @@
-import { LOGIN_START, LOGIN_RESOLVED } from "../actions";
+import {
+  LOGIN_START,
+  LOGIN_RESOLVED,
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE
+} from "../actions";
 
 const initialState = {
   isLoggingIn: false,
-  friends: []
+  friends: [],
+  error: "",
+  fetchingData: false
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -17,6 +25,31 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         isLoggingIn: false
+      };
+    }
+    case FETCH_DATA_START: {
+      return {
+        ...state,
+        error: "",
+        fetchingData: true
+      };
+    }
+    case FETCH_DATA_SUCCESS: {
+      //Log what is actually being fetched
+      console.log(action.payload);
+      return {
+        ...state,
+        error: "",
+        fetchingData: false,
+        friends: action.payload
+      };
+    }
+    case FETCH_DATA_FAILURE: {
+      console.log(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+        fetchingData: false
       };
     }
     default:
